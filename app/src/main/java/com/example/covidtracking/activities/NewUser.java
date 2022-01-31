14d1262153392cbf34afc92eb90ab8eb95d1a4c3
NiteshAru1998 <nitesharu1998@gmail.com>
2022-01-31 19:45:31 +0530

    some testing changes

--- app/src/main/java/com/example/covidtracking/activities/NewUser.java
+++ app/src/main/java/com/example/covidtracking/activities/NewUser.java
@@ -3,6 +3,7 @@ package com.example.covidtracking.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.covidtracking.R;
 
@@ -12,5 +13,7 @@ public class NewUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
+
+        Toast.makeText(this, "hello changes", Toast.LENGTH_SHORT).show();
     }
 }
