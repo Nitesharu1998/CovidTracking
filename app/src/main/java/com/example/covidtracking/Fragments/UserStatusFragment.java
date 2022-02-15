5f348b41c25e70df219c0608e837ed7b4c9132aa
nitesh <nitesharu1998@gmail.com>
2022-02-15 10:55:41 +0530

    working on the AssessmentActivity

--- app/src/main/java/com/example/covidtracking/Fragments/UserStatusFragment.java
+++ app/src/main/java/com/example/covidtracking/Fragments/UserStatusFragment.java
@@ -1,5 +1,7 @@
 package com.example.covidtracking.Fragments;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,19 +11,25 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.covidtracking.Adapters.HelplineAdapter;
 import com.example.covidtracking.ModelClasses.HelplineModel;
 import com.example.covidtracking.R;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.util.ArrayList;
 
 public class UserStatusFragment extends Fragment {
     RecyclerView rcl_numbers;
-    ArrayList<Integer> mainIcons =new ArrayList();
-    ArrayList<String> helplineTexts =new ArrayList();
-    ArrayList<String> helplineNumbers =new ArrayList();
-    ArrayList<HelplineModel.HelplineData> helplineData= new ArrayList<>();
+    ArrayList<Integer> mainIcons = new ArrayList();
+    ArrayList<String> helplineTexts = new ArrayList();
+    ArrayList<String> helplineNumbers = new ArrayList();
+    ArrayList<HelplineModel.HelplineData> helplineData = new ArrayList<>();
+
+    View root;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,20 +38,52 @@ public class UserStatusFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View root= inflater.inflate(R.layout.fragment_userstatus, container, false);
+
+        root = inflater.inflate(R.layout.fragment_userstatus, container, false);
         initUI(root);
         fillArraylists();
         setAdapter();
         return root;
     }
 
+    private void openAssessmentPopup(View root) {
+        BottomSheetDialog bottomDialog = new BottomSheetDialog(root.getContext());
+        bottomDialog.setContentView(R.layout.take_assessment);
+        bottomDialog.setCancelable(false);
+        getActivity().overridePendingTransition(android.R.style.Widget_DeviceDefault_Light_PopupWindow, android.R.id.accessibilityActionScrollDown);
+
+        Button btn_proceed;
+        TextView tv_skip;
+
+        btn_proceed = bottomDialog.findViewById(R.id.btn_proceed);
+        tv_skip = bottomDialog.findViewById(R.id.tv_skip);
+        btn_proceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(),AssessmentActivity.class));
+                bottomDialog.dismiss();
+            }
+        });
+        tv_skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bottomDialog.dismiss();
+            }
+        });
+
+
+        bottomDialog.show();
+    }
+
     private void setAdapter() {
         LinearLayoutManager manager = new LinearLayoutManager(getContext());
         rcl_numbers.setLayoutManager(manager);
-        HelplineAdapter adapter=new HelplineAdapter(getContext(),helplineData);
+        HelplineAdapter adapter = new HelplineAdapter(getContext(), helplineData);
         rcl_numbers.setAdapter(adapter);
+
+        openAssessmentPopup(root);
     }
+
 
     private void fillArraylists() {
         mainIcons.add(R.drawable.ic_support);
@@ -68,7 +108,7 @@ public class UserStatusFragment extends Fragment {
         helplineNumbers.add("9013151515");
 
         for (int i = 0; i < mainIcons.size(); i++) {
-            HelplineModel.HelplineData testdata=new HelplineModel.HelplineData();
+            HelplineModel.HelplineData testdata = new HelplineModel.HelplineData();
             testdata.setImage(mainIcons.get(i));
             testdata.setMaintext(helplineTexts.get(i));
             testdata.setNumber(helplineNumbers.get(i));
@@ -78,7 +118,7 @@ public class UserStatusFragment extends Fragment {
     }
 
     private void initUI(View root) {
-        rcl_numbers=root.findViewById(R.id.rcl_numbers);
+        rcl_numbers = root.findViewById(R.id.rcl_numbers);
 
     }
 }
---
+++ app/src/main/java/com/example/covidtracking/activities/AssessmentActivity.java
@@ -0,0 +1,16 @@
+package com.example.covidtracking.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.covidtracking.R;
+
+public class AssessmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assessment);
+    }
+}
